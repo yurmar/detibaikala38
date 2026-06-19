69fc9affdff480140bdc818f2275093e3d15490f
yurmar <yurmarchuk@gmail.com>
2026-06-19 15:45:17 +0800

    Fix style list

--- themes/deti-baikala/functions.php
+++ themes/deti-baikala/functions.php
@@ -5,7 +5,7 @@
 
 if ( ! defined( 'ABSPATH' ) ) exit;
 
-define( 'DB_THEME_VERSION', '1.0.5' );
+define( 'DB_THEME_VERSION', '1.0.6' );
 define( 'DB_THEME_DIR', get_template_directory() );
 define( 'DB_THEME_URI', get_template_directory_uri() );
 
